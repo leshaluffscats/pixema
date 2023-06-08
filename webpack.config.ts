b63875395f1c990d9e98560f1509ea7fef59d34f
leshaluffscats <alexanikeenko317@gmail.com>
2023-06-08 18:09:36 +0300

    refactor

--- webpack.config.ts
+++ webpack.config.ts
@@ -32,6 +32,12 @@ module.exports = {
         }),
         new webpack.DefinePlugin({
             "process.env": JSON.stringify(process.env),
+            "process.env.REACT_APP_FIREBASE_API_KEY": JSON.stringify(process.env.REACT_APP_FIREBASE_API_KEY),
+            "process.env.REACT_APP_FIREBASE_AUTH_DOMAIN": JSON.stringify(process.env.REACT_APP_FIREBASE_AUTH_DOMAIN),
+            "process.env.REACT_APP_FIREBASE_PROJECT_ID": JSON.stringify(process.env.REACT_APP_FIREBASE_PROJECT_ID),
+            "process.env.REACT_APP_FIREBASE_STORAGE_BUCKET": JSON.stringify(process.env.REACT_APP_FIREBASE_STORAGE_BUCKET),
+            "process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID": JSON.stringify(process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID),
+            "process.env.REACT_APP_FIREBASE_APP_ID": JSON.stringify(process.env.REACT_APP_FIREBASE_APP_ID),
         }),
     ],
     module: {
